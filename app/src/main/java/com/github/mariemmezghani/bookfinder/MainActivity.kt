fa358154b3dc8835c6d9053bb838c79ff879684b
MariemMezghani <35550711+MariemMezghani@users.noreply.github.com>
2022-06-30 15:11:19 +0200

    add children tag to ads

--- app/src/main/java/com/github/mariemmezghani/bookfinder/MainActivity.kt
+++ app/src/main/java/com/github/mariemmezghani/bookfinder/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.github.mariemmezghani.bookfinder.databinding.ActivityMainBinding
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
+import com.google.android.gms.ads.RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,6 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // initialize adMob
+        val conf = RequestConfiguration.Builder()
+            .setTagForChildDirectedTreatment(TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE)
+            .build()
+        MobileAds.setRequestConfiguration(conf)
         MobileAds.initialize(this) {}
 
     }
